3d3568a147c037178f70ba40385ca9d10c2673a1
twang281314 <twang281314@gmail.com>
2016-02-03 16:19:52 +0800

    sb admin react

---
+++ sb-admin-react-master/src/components/pages/Logout.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var Router = require('react-router');
+var Reflux = require('reflux');
+
+var LogoutPage = React.createClass({
+    
+  componentWillMount: function(){
+
+  },
+  
+  mixins: [Router.Navigation],
+
+  render: function(){
+
+  		return <div></div>;
+  }
+
+});
+
+export default LogoutPage;
